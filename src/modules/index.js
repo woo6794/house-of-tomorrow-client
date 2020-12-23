3ad840cf7b5c5b6ae98a74da7babb608ee437374
Nomad_Hash <badsoap141@gmail.com>
2020-12-23 19:04:26 +0900

    feature / 로그인 모달 및 카카오톡 로그인 기능 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
+import loginReducer, { loginWatcherSaga } from './systems/user_login/login';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ loginReducer });
 export function* rootSaga() {
-  yield all([]);
+  yield all([loginWatcherSaga()]);
 }
 
 export default rootReducer;
